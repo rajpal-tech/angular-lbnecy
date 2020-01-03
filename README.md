ad8a0bf90a5c2634925ae98bc19702b2acf80a32
rajpal-tech <rajpaltechugo@gmail.com>
2020-01-04 00:55:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lbnecy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lbnecy)
---
+++ src/app/rajpal/rajpal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rajpal',
+  templateUrl: './rajpal.component.html',
+  styleUrls: ['./rajpal.component.css']
+})
+export class RajpalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
